4951ba2192326fdfefc2661f5a2346f898ea9a53
avganka <karambasterg@gmail.com>
2022-11-06 01:21:33 +0300

    search fix

--- src/js/modules/searchToggle.js
+++ src/js/modules/searchToggle.js
@@ -1,21 +1,42 @@
 const closeBtn = document.querySelector('.search__close-icon');
 const searchBtn = document.querySelector('.search__search-icon');
-const phoneBtn = document.querySelector('.header__icon--phone');
 const searchBlock = document.querySelector('.header__search');
-const feedbackBtn = document.querySelector('.header__contacts-button');
+const phoneBtn = document.querySelector('.header__icon--phone');
+const feedbackBtn = document.querySelector('.header__feedback-button ');
+
+const timeouts = [];
+
+const actionOnTimeout = (callback, time) => {
+  const timeout = setTimeout(() => {
+    callback();
+  }, time);
+  timeouts.push(timeout);
+};
+
+const onSearchOpen = () => {
+  searchBlock.classList.add('active');
+};
+
+const onSearchClose = () => {
+  feedbackBtn.style.display = 'block';
+  phoneBtn.style.display = 'block';
+};
 
 searchBtn.addEventListener('click', () => {
+  timeouts.forEach((timeout) => clearTimeout(timeout));
   if (!searchBlock.classList.contains('active')) {
-    searchBlock.classList.add('active');
-    feedbackBtn.style.display = '0';
-    phoneBtn.style.display = '0';
+    feedbackBtn.style.display = 'none';
+    phoneBtn.style.display = 'none';
+
+    actionOnTimeout(onSearchOpen, 0);
   }
 });
 
 closeBtn.addEventListener('click', () => {
+  timeouts.forEach((timeout) => clearTimeout(timeout));
   if (searchBlock.classList.contains('active')) {
     searchBlock.classList.remove('active');
-    feedbackBtn.style.display = 'block';
-    phoneBtn.style.display = 'block';
+
+    actionOnTimeout(onSearchClose, 400);
   }
 });
