c15f722b1740618b61a7fb0a0c57b7ea2922e7b7
avganka <karambasterg@gmail.com>
2022-11-09 02:58:33 +0300

    add cookies popup

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import '../scss/style.scss';
 import './modules/menuToggle.js';
 import './modules/searchToggle.js';
 import './modules/popup.js';
+import './modules/cookies.js';
 
 document.addEventListener('DOMContentLoaded', () => {});
---
+++ src/js/modules/cookies.js
@@ -0,0 +1,43 @@
+const cookiesSuccessBtn = document.querySelector('.cookies__btn');
+const cookiesBanner = document.querySelector('.cookies');
+
+const domain = window.location.hostname;
+const path = '/';
+const maxAge = 10;
+
+const getCookie = (name) => {
+  let matches = document.cookie.match(
+    new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)')
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+};
+
+const setCookie = (name, value, options = {}) => {
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += '; ' + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += '=' + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+};
+
+const showCookiesBanner = getCookie('allowCookies');
+
+if (!showCookiesBanner) {
+  setTimeout(() => {
+    cookiesBanner.classList.add('opened');
+  }, 1000);
+  cookiesSuccessBtn.addEventListener('click', () => {
+    setCookie('allowCookies', true, {path, domain, 'max-age': maxAge});
+    cookiesBanner.classList.remove('opened');
+  });
+}
